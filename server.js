61c5e85d2bd4ef03a64c67cdf111450cac429225
Adham EL Banhawy <the.benhawy@gmail.com>
2017-10-10 23:18:35 -0500

    Added connection to db in server logic

--- server.js
+++ server.js
@@ -1,15 +1,41 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const shortid = require('shortid');
+const validUrl = require('valid-url');
+// const env = process.env.MONGODB_URI;
+app.use(express.static('public'))
 
-var app = express();
-
-app.get('/:url(*)', (req,res)=>{
-    console.log('Working');
-    var url = req.params.url
-    res.json({
-        message: "Hola",
-        url : url
-    })
+// Homepage
+app.get('/', (req,res)=>{
+    req.render('index')
 })
+
+app.get('/new/:url(*)', (req,res)=>{
+    console.log('Working');
+    // Connect to DB
+    MongoClient.connect(process.env.MONGODB_URI, (err,db)=>{
+        if (err) console.log('Unable to connect to the mongoDB server. Error:', err);
+        let collection = db.collection('urls');
+        console.log('Connection Established to db.');
+        const url = req.params.url;
+        if (validUrl.isUri(url)){
+            const hash = shortid.generate();
+            const newUrl = { url : url, alias: hash };
+            collection.insert([newUrl]);
+            res.json({
+                original_url: url,
+                alias_url : 'localhost:3000/' + hash
+            })
+        } else{
+            res.json({ error: "Wrong url format, make sure you have a valid protocol and real site." });
+        }
+        collection.insert([insertLink]);
+        db.close();
+    });
+});
 
 app.listen(3000);
